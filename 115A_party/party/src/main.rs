0eac0d79afcaa1d0b7dc7543229ff9ec07f39c08
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2020-06-21 22:41:13 +0300

    asd

--- 115A_party/party/src/main.rs
+++ 115A_party/party/src/main.rs
@@ -30,30 +30,31 @@ fn main() {
         }
     }
 
-    let ref depth = 1;
+    let mut depth = 1;
 
-    iterate_tree_depth(&&main_boss_id_vec, &&main_vec, *depth);
+    iterate_tree_depth(&&main_boss_id_vec, &&main_vec, &mut depth);
     println!("{:?}", depth);
 }
 
-fn iterate_tree_depth(middle_vector: &Vec<i32>, people_vec: &Vec<i32>, mut depth: i32) {
+fn iterate_tree_depth(boss_vector: &Vec<i32>, people_vec: &Vec<i32>, depth: &mut i32) {
     let mut all_length_is_null: bool = true;
-    let a = middle_vector.len();
+    let a = boss_vector.len();
 
     for i in 0..a {
-        let y_vec = people_vec
+        let worker_vec = people_vec
             .iter()
-            .filter(|&&x| x == (middle_vector[i] as i32))
+            .filter(|&&x| x == (boss_vector[i] as i32))
             .cloned()
             .collect::<Vec<i32>>();
 
-        if y_vec.len() > 1 {
+        if worker_vec.len() > 0 {
+            println!("ho");
             all_length_is_null = false;
-            iterate_tree_depth(&&y_vec, &&people_vec, depth);
+            iterate_tree_depth(&&worker_vec, &&people_vec, depth);
         }
     }
 
     if !all_length_is_null {
-        depth += 1;
+        *depth = *depth + 1;
     }
 }
